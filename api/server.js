689ab9e2a8a4c0bde59b1c5d756344f4ce97c4d4
Duncan McDowell <mcdowell.duncan@gmail.com>
2017-12-21 16:02:13 -0500

    majority of server refactoring is complete

--- api/server.js
+++ api/server.js
@@ -7,64 +7,104 @@ var moment     = require('moment');
 var cors       = require('cors')
 var WebSocket  = require('ws');
 const http     = require('http');
-mongoose.connect('mongodb://glance:NHM-r6U-t5m-nby@ds159856.mlab.com:59856/glance');
+mongoose.connect('mongodb://glance:NHM-r6U-t5m-nby@ds159856.mlab.com:59856/glance', {useMongoClient: true});
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors())
 
-var port = process.env.PORT || 8080;
-var router = express.Router();
+let port = process.env.PORT || 8080;
+let router = express.Router();
 
 // Schema
 
-var Schema = mongoose.Schema;
-var historicalPricePointSchema = new Schema({
+let Schema = mongoose.Schema;
+let historicalPricePointSchema = new Schema({
   price: Number,
   date: Number,
   formattedDate: String,
   newsItem: Schema.Types.Mixed
 });
 
-var currentPriceSchema = new Schema({
+let currentPriceSchema = new Schema({
   price: Number,
   date: String
 })
 
+let lastBlockSchema = new Schema({
+  value: Number,
+  date: String
+})
+
+let uncomfirmedTransactionsSchema = new Schema({
+  value: Number,
+  health: Schema.Types.Mixed,
+  date: String
+})
+
+let gasPriceSchema = new Schema({
+  price: Number,
+  health: Schema.Types.Mixed,
+  date: String
+})
+
 let CurrentPrice = mongoose.model('CurrentPrice', currentPriceSchema);
+let LastBlock = mongoose.model('LastBlock', lastBlockSchema);
+let UncomfirmedTransactions = mongoose.model('UncomfirmedTransactions', uncomfirmedTransactionsSchema);
+let GasPrice = mongoose.model('GasPrice', gasPriceSchema);
 
 
-// Sockets
+// Sockets and polling functions
 
 const wss = new WebSocket.Server({ port: 9090 });
 
 wss.on('connection', function connection(ws) {
 
-  Api.getLatestPrice().then(function(price) {
-    ws.send(JSON.stringify({currentPrice: price}));
-  });
+  websocketHelper(ws);
+  // update client every five seconds
   setInterval(function() {
-    Api.getLatestPrice().then(function(price) {
-      ws.send(JSON.stringify({currentPrice: price}));
-    });
-  },7000)
-
-  // getGasDetails().then(function(result) {
-  //   // if (result.price != lastGasPrice) {
-  //     ws.send(JSON.stringify({gasDetails: result}));
-  //   // }
-  // });
-  //
-  // getUncomfirmedTransactions().then(function(result) {
-  //   ws.send(JSON.stringify({uncomfirmedTransactions: result}));
-  // });
-  //
-  // getLastBlock().then(function(result) {
-  //   ws.send(JSON.stringify({latestBlock: result}));
-  // })
+    websocketHelper(ws);
+  },5000)
 
 });
+
+// Update DB every ten seconds
+
+setInterval(function() {
+  ExtApi.getCurrentEthPrice().then(function(result) {
+    CurrentPrice.create({price: result, date: new Date()}, function(err, price) {
+      if (err) {
+        console.log('failure saving current price data: ', err);
+      }
+    });
+  });
+
+  ExtApi.getLastBlock().then(function(result) {
+    LastBlock.create({value: result, date: new Date()}, function(err, block) {
+      if (err) {
+        console.log('failure saving last block data: ', err);
+      }
+    });
+  });
+
+  ExtApi.getGasDetails().then(function(result) {
+    GasPrice.create({price: result.price, health: result.health, date: new Date()}, function(err, block) {
+      if (err) {
+        console.log('failure saving gas price data: ', err);
+      }
+    });
+  });
+
+  ExtApi.getUncomfirmedTransactions().then(function(result) {
+    UncomfirmedTransactions.create({value: result.value, health: result.health, date: new Date()}, function(err, block) {
+      if (err) {
+        console.log('failure saving gas price data: ', err);
+      }
+    });
+  });
+
+}, 10000);
 
 // First-party API functions
 
@@ -78,104 +118,117 @@ class Api {
       console.log(err);
     }
   }
+  static async getLastBlock() {
+    try {
+      let query = await LastBlock.findOne().sort({ field: 'asc', _id: -1 }).limit(1).select('value').exec();
+      return query.value;
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
+  static async getGasPrice() {
+    try {
+      let query = await GasPrice.findOne().sort({ field: 'asc', _id: -1 }).limit(1).select('price health').exec();
+      return query;
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
+  static async getUncomfirmedTransactions() {
+    try {
+      let query = await UncomfirmedTransactions.findOne().sort({ field: 'asc', _id: -1 }).limit(1).select('value health').exec();
+      return query;
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
 }
-
 
 // Third-party API functions
 
-let parityEndpoint = "https://wallet.parity.io:8545"
+class ExtApi {
 
-getHistoricalPrice = function() {
-  return axios.get('https://poloniex.com/public', {
-    params: {
-      command:'returnChartData',
-      currencyPair: 'USDT_ETH',
-      start: 1511281600,
-      end: 9999999999,
-      period: 14400
-    }
-  })
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-      return error
-    });
-}
-
-getNewsItems = function() {
-  return axios.get('http://webhose.io/filterWebContent?token=8b783afd-c348-436d-8913-1e1450d86f00&format=json&ts=1511830330016&sort=crawled&q=ethereum%20social.facebook.shares%3A%3E5000')
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      return {'error':error};
-    });
-}
-
-getCurrentEthPrice = function() {
-  return axios.get('https://poloniex.com/public', {
-    params: {
-      command:'returnTicker'
-    }
-  })
-    .then(function (response) {
-      if (response.data) {
-        return parseFloat(response.data['USDT_ETH'].last,10).toFixed(2);
+  static getHistoricalPrice() {
+    return axios.get('https://poloniex.com/public', {
+      params: {
+        command:'returnChartData',
+        currencyPair: 'USDT_ETH',
+        start: 1511281600,
+        end: 9999999999,
+        period: 14400
       }
     })
-    .catch(function (error) {
-      // likely a timeout
-    });
-}
-
-setInterval(function() {
-  getCurrentEthPrice().then(function(result) {
-    CurrentPrice.create({price: result, date: new Date()}, function(err, pricePoints) {
-      if (err) {
-        console.log('failure saving current price data: ', err);
-      }
-    });
-  });
-}, 7000);
-
-// TODO implement try/catch for these async awaits
-getGasDetails = async function() {
-  let response = await axios.post(parityEndpoint, {"method":"eth_gasPrice","params":[],"id":1,"jsonrpc":"2.0"})
-  let result = await response;
-  if (result.data) {
-    parsedHex = parseInt(result.data.result, 16);
-    let gwei = parsedHex / 1000000000 // convert from wei
-    lastGasPrice = gwei; // this is ephemeral, could be moved to DB if necessary
-    let health = determineHealth(gwei, 28, 60);
-    return {price: gwei, health: health};
+      .then(function (response) {
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+        return error
+      });
   }
-}
 
-getLastBlock = async function () {
-  let response = await axios.post(parityEndpoint, {"method":"eth_getBlockByNumber","params":['latest', false],"id":1,"jsonrpc":"2.0"})
-  let result = await response;
-  if (result.data) {
-    parsedHex = parseInt(result.data.result.number, 16);
-    return parsedHex;
+  static async getNewsItems() {
+    let response = await axios.get('http://webhose.io/filterWebContent?token=8b783afd-c348-436d-8913-1e1450d86f00&format=json&ts=1511830330016&sort=crawled&q=ethereum%20social.facebook.shares%3A%3E5000')
+    let result = await response;
+    try {
+      return result.data;
+    }
+    catch (err) {
+      // implement sentry
+    }
   }
-}
 
-getUncomfirmedTransactions = async function () {
-  let response = await axios.get('https://api.blockcypher.com/v1/eth/main')
-  let result = await response;
-  if (result.data) {
-    let health = determineHealth(result.data.unconfirmed_count, 100000, 180000);
-    return {value: result.data.unconfirmed_count, health: health};
+  static async getCurrentEthPrice() {
+    let response = await axios.get('https://poloniex.com/public', {params: {command:'returnTicker'}})
+    let result = await response;
+    try {
+      return parseFloat(result.data['USDT_ETH'].last,10).toFixed(2);
+    }
+    catch(err) {
+      // implement sentry
+    }
+  }
+
+  static async getGasDetails() {
+    let response = await axios.post(parityEndpoint, {"method":"eth_gasPrice","params":[],"id":1,"jsonrpc":"2.0"})
+    let result = await response;
+    if (result.data) {
+      let parsedHex = parseInt(result.data.result, 16);
+      let gwei = parsedHex / 1000000000 // convert from wei
+      let lastGasPrice = gwei; // this is ephemeral, could be moved to DB if necessary
+      let health = determineHealth(gwei, 28, 60);
+      return {price: gwei, health: health};
+    }
+  }
+
+  static async getLastBlock() {
+    let response = await axios.post(parityEndpoint, {"method":"eth_getBlockByNumber","params":['latest', false],"id":1,"jsonrpc":"2.0"})
+    let result = await response;
+    if (result.data) {
+      let parsedHex = parseInt(result.data.result.number, 16);
+      return parsedHex;
+    }
+  }
+
+  static async getUncomfirmedTransactions() {
+    let response = await axios.get('https://api.blockcypher.com/v1/eth/main')
+    let result = await response;
+    if (result.data) {
+      let health = determineHealth(result.data.unconfirmed_count, 100000, 180000);
+      return {value: result.data.unconfirmed_count, health: health};
+    }
   }
 }
 
 // Helper functions and variables
 
 let lastGasPrice = 0;
+let parityEndpoint = "https://wallet.parity.io:8545"
 
-const determineHealth = function(value, low, middle) {
+let determineHealth = function(value, low, middle) {
   if (value < low) {
     return {'status':'healthy', 'value':'#73d13d'};
   } else if (value > low && value < middle) {
@@ -183,6 +236,21 @@ const determineHealth = function(value, low, middle) {
   } else {
     return {'status':'unhealthy', 'value':'#ff4d4f'};
   }
+}
+
+let websocketHelper = function(ws) {
+  Api.getLatestPrice().then(function(price) {
+    ws.send(JSON.stringify({currentPrice: price}));
+  });
+  Api.getLastBlock().then(function(block) {
+    ws.send(JSON.stringify({latestBlock: block}));
+  });
+  Api.getGasPrice().then(function(gasDetails) {
+    ws.send(JSON.stringify({gasDetails: gasDetails}));
+  });
+  Api.getUncomfirmedTransactions().then(function(uncomfirmedTransactions) {
+    ws.send(JSON.stringify({uncomfirmedTransactions: uncomfirmedTransactions}));
+  });
 }
 
 saveHistoricalData = function() {
@@ -247,4 +315,4 @@ router.route('/historical')
 app.use('/api', router);
 
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('listening on port ' + port);
